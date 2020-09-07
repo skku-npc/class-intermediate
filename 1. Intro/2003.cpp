94d7e1e75c4b8d34ce9c5d136ee5fce506b05e72
Jaemin Choi <1dotolee@gmail.com>
2020-09-07 20:03:18 +0900

    Fix 2003 code

--- 1. Intro/2003.cpp
+++ 1. Intro/2003.cpp
@@ -3,23 +3,24 @@ using namespace std;
 
 int n, m;
 int ans;
-int arr[10005];
+int a[10005];
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        cin >> arr[i];
-        arr[i] += arr[i - 1];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
 
-    int l = 0, r = 1;
-    while (r <= n) {
-        if (arr[r] - arr[l] == m) ans++;
-        if (arr[r] - arr[l] <= m) {
-            r++;
+    int l = 0, r = 0;
+    int sum = a[0];
+
+    while (r < n) {
+        if (sum == m) ans++;
+        if (sum > m) {
+            sum -= a[l++];
         } else {
-            l++;
+            sum += a[++r];
         }
     }
 
