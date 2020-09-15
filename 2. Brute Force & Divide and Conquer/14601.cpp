bee4f89659cf14f466766866fb1f6ee3b68d7cfb
Jaemin Choi <1dotolee@gmail.com>
2020-09-15 20:35:12 +0900

    14601번 풀이 formatting

---
+++ 2. Brute Force & Divide and Conquer/14601.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int res[133][133];  // 타일 칠해진 결과를 저장
+int color = 1;      // 끼울 타일
+
+void f(int x, int y, int p2, pair<int, int> hole) {
+    if (p2 == 0) return;  // 기저 조건
+
+    int now_color = color++;
+
+    //분할과정
+    int nxt_p2 = p2 - 1;
+    int nxt_sz = 1 << nxt_p2;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            int nx = x + i * nxt_sz;
+            int ny = y + j * nxt_sz;
+            if (nx <= hole.first && hole.first < nx + nxt_sz &&
+                ny <= hole.second && hole.second < ny + nxt_sz) {
+                f(nx, ny, nxt_p2, hole);
+            } else {
+                int mid_x = x + nxt_sz - 1 + i;
+                int mid_y = y + nxt_sz - 1 + j;
+                f(nx, ny, nxt_p2, make_pair(mid_x, mid_y));
+                res[mid_x][mid_y] = now_color;
+            }
+        }
+    }
+}
+
+int main() {
+    pair<int, int> p;
+    int k;
+    cin >> k >> p.second >> p.first;
+    p.first--, p.second--;  // 0-based index
+    f(0, 0, k, p);
+    for (int i = (1 << k) - 1; i >= 0; --i) {
+        for (int j = 0; j < (1 << k); ++j) {
+            cout << (res[i][j] ? res[i][j] : -1) << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
