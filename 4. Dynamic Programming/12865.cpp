c04db244d948a7e73cf7556bf8196021b598bca0
leesj <31756944+leesj10147@users.noreply.github.com>
2020-10-13 08:28:55 +0900

    Update 12865.cpp

--- 4. Dynamic Programming/12865.cpp
+++ 4. Dynamic Programming/12865.cpp
@@ -1,25 +1,23 @@
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
-int max(int a, int b){return a>b?a:b;}
-int min(int a, int b){return a>b?b:a;}
-int n, k, w, v;
-int dp[2][100'001];
-int main()
-{
-    scanf("%d%d", &n, &k);
 
-    int ans = 0;
-    for (int i=1;i<=n;++i)
-    {
-        scanf("%d%d", &w, &v);
-        for (int j=k-w;j>=0;--j)
-            dp[i&1][j+w] = max(dp[(i-1)&1][j+w], dp[(i-1)&1][j]+v);
-        for (int j = min(w-1, k); j >= 0; --j)
-            dp[i&1][j] = dp[(i-1)&1][j];
-    }
-    for (int j = 0; j <= k;++j) ans = max(ans, dp[n&1][j]);
-    printf("%d\n", ans);
-    return 0;
+int n, k, ans;
+int dp[100005];
+
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0);
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		int w, v;
+		cin >> w >> v;
+		for (int j = k - w; j >= 0; j--) {
+			dp[j + w] = max(dp[j + w], dp[j] + v);
+		}
+	}
+	for (int i = 0; i <= k; i++) {
+		ans = max(ans, dp[i]);
+	}
+	cout << ans << endl;
 }
 /*
  dp[i][j] = 1~i 물건들을 고려했고, 무게가 j일 때 최대 가치
